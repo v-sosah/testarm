2367c9757f36aecd511d80462850315884e05dec
v-sosah <43109112+v-sosah@users.noreply.github.com>
2021-01-20 16:12:29 +0530

    updated the appregistration ps

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F101-storage-account-create%2Fazuredeploy.json)
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fv-sosah%2Ftestarm%2Fmain%2Fdeploy.json)
---
+++ appregistration.ps1
@@ -0,0 +1,10 @@
+$appName = "skstestapp"
+$appURI = "https://tailwindtraderssalesapp.twtmitt.onmicrosoft.com"
+$appHomePageUrl = "http://www.tailwindtraders.com/"
+$appReplyURLs = @($appURI, $appHomePageURL, "https://localhost:1234")
+Install-Module AzureAD
+Connect-AzureAD
+if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"  -ErrorAction SilentlyContinue))
+{
+    $myApp = New-AzureADApplication -DisplayName $appName -IdentifierUris $appURI -Homepage $appHomePageUrl -ReplyUrls $appReplyURLs    
+}
