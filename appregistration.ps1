34372e9a5bc3ee4c1cfb2ada9cfc2fa929b0ca5f
v-sosah <43109112+v-sosah@users.noreply.github.com>
2021-01-21 19:07:39 +0530

    update

--- appregistration.ps1
+++ appregistration.ps1
@@ -2,8 +2,9 @@ $appName = "skstestapp"
 $appURI = "https://tailwindtraderssalesapp.twtmitt.onmicrosoft.com"
 $appHomePageUrl = "http://www.tailwindtraders.com/"
 $appReplyURLs = @($appURI, $appHomePageURL, "https://localhost:1234")
-UnInstall-Module AzureAD
+Install-Module AzureAD
 Install-Module AzureADPreview
+Import-Module AzureAD
 if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"  -ErrorAction SilentlyContinue))
 {
     $myApp = New-AzureADApplication -DisplayName $appName -IdentifierUris $appURI -Homepage $appHomePageUrl -ReplyUrls $appReplyURLs    
